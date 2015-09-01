e49a69262b9f4c53028bf1dbf7b4e557b47c594c
Administrator <Administrator@ET-JIANGUO.uoko.com>
2015-09-01 15:58:39 +0800

    修改实务实现

--- test-spring-boot/src/main/java/test/controller/UserController.java
+++ test-spring-boot/src/main/java/test/controller/UserController.java
@@ -1,11 +1,9 @@
 package test.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import test.entity.User;
 import test.service.UserService;
 
 @RestController
@@ -15,11 +13,9 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
-	@Transactional
 	@RequestMapping("add")
 	public String add() {
-		userService.addUser(new User("fuc", 29));
-		userService.addUser(new User("jm", 26));
+		userService.addUser();
 		return "success";
 	}
 	
--- test-spring-boot/src/main/java/test/service/UserService.java
+++ test-spring-boot/src/main/java/test/service/UserService.java
@@ -2,6 +2,7 @@ package test.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import test.dao.UserMapper;
 import test.entity.User;
@@ -11,7 +12,9 @@ public class UserService {
 	@Autowired
 	private UserMapper userMapper;
 	
-	public void addUser(User user) {
-		userMapper.addUser(user);
+	@Transactional
+	public void addUser() {
+		userMapper.addUser(new User("fc", 29));
+		userMapper.addUser(new User("jm", 26));
 	}
 }
