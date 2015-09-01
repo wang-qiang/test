4f88b515040eb7ede652dcae488d1698e07879a9
Administrator <Administrator@ET-JIANGUO.uoko.com>
2015-09-01 15:54:33 +0800

    加上事物

--- test-spring-boot/src/main/java/test/controller/UserController.java
+++ test-spring-boot/src/main/java/test/controller/UserController.java
@@ -1,6 +1,7 @@
 package test.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +15,11 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@Transactional
 	@RequestMapping("add")
 	public String add() {
 		userService.addUser(new User("fuc", 29));
+		userService.addUser(new User("jm", 26));
 		return "success";
 	}
 	
